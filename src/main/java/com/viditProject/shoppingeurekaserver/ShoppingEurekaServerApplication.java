0f461feb55acec5870c727b9bcfdc7e7ae352abb
Vidit <viditsaxena98@gmail.com>
2021-10-25 12:12:50 +0530

    Initial commit

---
+++ src/main/java/com/viditProject/shoppingeurekaserver/ShoppingEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.viditProject.shoppingeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ShoppingEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingEurekaServerApplication.class, args);
+	}
+
+}
